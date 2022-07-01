ae4bd0c450ff6d52f93183ee3095de1757b34bdf
xianghu <350971131@qq.com>
2022-07-01 09:26:15 +0800

    jjj

--- app/src/main/java/vpn/tydic/com/myapplication/MainActivity.java
+++ app/src/main/java/vpn/tydic/com/myapplication/MainActivity.java
@@ -1,25 +1,11 @@
 package vpn.tydic.com.myapplication;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Handler;
-import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-
-import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
 
 public class MainActivity extends Activity {
-    //分支master
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
